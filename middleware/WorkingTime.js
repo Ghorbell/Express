b169ec1739df90e3b445cca7e7a693f6ee4c3f2e
Ghorbell <75613863+Ghorbell@users.noreply.github.com>
2021-09-16 17:28:36 +0100

    Add files via upload

--- middleware/WorkingTime.js
+++ middleware/WorkingTime.js
@@ -3,7 +3,7 @@ const workinghours = (req, res, next) => {
   let date = new Date();
   let days = date.getDay();
   let hours = date.getHours();
-  if (days == 0 || days == 5 /*|| hours < 9 || hours > 17*/) {
+  if (days == 0 || days == 5 || hours < 9 || hours > 17) {
     res.sendFile("closed.html", { root: __dirname });
   } else {
     next();
